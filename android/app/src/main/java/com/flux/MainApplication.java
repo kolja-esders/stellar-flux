dd2d35bda676a3e24c349d78533097d7a98b1c50
Kolja Esders <kolja.esders@gmail.com>
2018-02-06 16:51:12 +0100

    Added touch id

--- android/app/src/main/java/com/flux/MainApplication.java
+++ android/app/src/main/java/com/flux/MainApplication.java
@@ -3,6 +3,7 @@ package com.flux;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfingerprint.FingerprintAuthPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new FingerprintAuthPackage(),
               new SvgPackage(),
               new StellarAccountPackage()
       );
