18c089bbda0c4ca5aca6482dd8fa787834c24605
Kolja Esders <kolja.esders@gmail.com>
2018-02-09 02:11:27 +0100

    Added qr code package

--- android/app/src/main/java/com/flux/MainApplication.java
+++ android/app/src/main/java/com/flux/MainApplication.java
@@ -3,13 +3,13 @@ package com.flux;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.rnfingerprint.FingerprintAuthPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.flux.modules.StellarAccountPackage;
-import com.horcrux.svg.SvgPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
-            new FingerprintAuthPackage(),
               new SvgPackage(),
+              new FingerprintAuthPackage(),
               new StellarAccountPackage()
       );
     }
