71133a0b17bdc4c1be5fccaf810eac8555ad145e
Kolja Esders <kolja.esders@gmail.com>
2018-02-09 02:38:15 +0100

    Increase brightness on QR code touch

--- android/app/src/main/java/com/flux/MainApplication.java
+++ android/app/src/main/java/com/flux/MainApplication.java
@@ -3,6 +3,7 @@ package com.flux;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.capslock.RNDeviceBrightness.RNDeviceBrightness;
 import com.horcrux.svg.SvgPackage;
 import com.rnfingerprint.FingerprintAuthPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new RNDeviceBrightness(),
               new SvgPackage(),
               new FingerprintAuthPackage(),
               new StellarAccountPackage()
--- pages/ReceivePage.js
+++ pages/ReceivePage.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableNativeFeedback, AsyncStorage, ActivityIndicator,  Platform } from 'react-native';
-import { Button } from 'react-native-common';
+import { StyleSheet, Text, View, ScrollView, Dimensions, Platform, AsyncStorage, TouchableOpacity } from 'react-native';
 import Svg, { Path, RadialGradient, Rect, Defs, Stop } from 'react-native-svg';
-import StellarAccount from '../helper/StellarAccount';
-import FormattingUtils from '../helper/FormattingUtils';
-import Transaction from '../components/Transaction';
+import QRCode from 'react-native-qrcode-svg';
+import DeviceBrightness from 'react-native-device-brightness';
 
 export default class ReceivePage extends React.Component {
+
+  state = { accountId: '', isBrightnessIncreased: false, originalBrightness: 0 }
   
   async componentWillMount() {
     let accountId = await AsyncStorage.getItem('@Flux:accountId')
-    console.log('accountId: ', accountId)
-    let balance = await StellarAccount.getBalance(accountId)
-    this.setState({ balance, isLoadingBalance: false })
+    this.setState({ accountId })
+  }
+
+  onCodePress = async () => {
+    if (this.state.isBrightnessIncreased) {
+      return
+    }
+    let originalBrightness = await DeviceBrightness.getBrightnessLevel()
+    this.setState({ originalBrightness, isBrightnessIncreased: true }, () => {
+      DeviceBrightness.setBrightnessLevel(1);
+      setTimeout(() => {
+        DeviceBrightness.setBrightnessLevel(originalBrightness);
+        this.setState({ isBrightnessIncreased: false })
+      }, 3000)
+    })
+  }
+
+  componentWillUnmount() {
+    DeviceBrightness.setBrightnessLevel(this.state.originalBrightness);
   }
 
   render() {
     let { height, width } = Dimensions.get('window');
     return (
-      <View style={styles.container}>
-        <Text>receive</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Svg style={styles.svgContainer} height={height} width={width}>
+          <Defs>
+            <RadialGradient id="grad" cx="100%" cy="150%" rx={2*height} ry={2*height} fx="50%" fy="50%" gradientUnits="userSpaceOnUse">
+              <Stop offset="0%" stopColor="#002348" stopOpacity="1" />
+              <Stop offset="100%" stopColor="#08B5E5" stopOpacity="1" />
+            </RadialGradient>
+          </Defs>
+          <Rect x="0" y="0" width={width} height={height} fill="url(#grad)" />
+        </Svg>
+        <TouchableOpacity style={styles.codeContainer} activeOpacity={1} onPress={this.onCodePress}>
+          <QRCode
+            color='#444'
+            size={width * 0.618}
+            value="GA6NL6C2IDWOPTB6USS4O3VRIODUDXIZPCIFZXA2YG7MNS6M3ORE3JBE"
+          />
+        </TouchableOpacity>
+        <Text style={styles.scanDesc}>Scan me!</Text>
+      </ScrollView>
     );
   }
 }
@@ -33,4 +65,13 @@ const styles = StyleSheet.create({
   svgContainer: {
     position: 'absolute',
   }, 
+  codeContainer: {
+    marginTop: '14.6%',
+    padding: '5%',
+    borderRadius: 3,
+    backgroundColor: '#fff',
+    elevation: 1
+  },
+  scanDesc: {
+  }
 });
