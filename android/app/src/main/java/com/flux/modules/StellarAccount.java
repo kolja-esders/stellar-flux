6946f2615004a80f369fd007bd78ef72bf69fd77
Kolja Esders <kolja.esders@gmail.com>
2018-02-09 02:14:54 +0100

    Method to send lumens with event feedback

--- android/app/src/main/java/com/flux/modules/StellarAccount.java
+++ android/app/src/main/java/com/flux/modules/StellarAccount.java
@@ -5,20 +5,51 @@ import android.util.Log;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
+import org.stellar.sdk.Account;
+import org.stellar.sdk.AssetTypeNative;
 import org.stellar.sdk.KeyPair;
+import org.stellar.sdk.Memo;
+import org.stellar.sdk.Network;
+import org.stellar.sdk.PaymentOperation;
 import org.stellar.sdk.Server;
+import org.stellar.sdk.Transaction;
 import org.stellar.sdk.responses.AccountResponse;
+import org.stellar.sdk.responses.SubmitTransactionResponse;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Scanner;
 
+import javax.annotation.Nullable;
+
 public class StellarAccount extends ReactContextBaseJavaModule {
+
+  private static final String HORIZON_TESTNET_URL = "https://horizon-testnet.stellar.org";
+
+  private static final String E_AMOUNT_TOO_SMALL = "E_AMOUNT_TOO_SMALL";
+
+  private static final String E_DESTINATION_DOES_NOT_EXIT = "E_DESTINATION_DOES_NOT_EXIT";
+
+  private static final String E_ACCOUNT_DOES_NOT_EXIT = "E_ACCOUNT_DOES_NOT_EXIT";
+
+  private static final String EV_SOURCE_ACCOUNT_UPDATED = "EV_SOURCE_ACCOUNT_UPDATED";
+
+  private static final String EV_DESTINATION_ACCOUNT_CHECKED = "EV_DESTINATION_ACCOUNT_CHECKED";
+
+  private static final String EV_CREATED_TRANSACTION = "EV_CREATED_TRANSACTION";
+
+  private String mSecretKey;
+
+  private String mAccountId;
+
+  private KeyPair mKeyPair;
 
   StellarAccount(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -27,6 +58,80 @@ public class StellarAccount extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "StellarAccount";
+  }
+
+  @ReactMethod
+  public void initializeWithSecretKey(final String secretKey) {
+    mSecretKey = secretKey;
+    mKeyPair = KeyPair.fromSecretSeed(secretKey);
+  }
+
+  @ReactMethod
+  public void initializeWithAccountId(final String accountId) {
+    mAccountId = accountId;
+    mKeyPair = KeyPair.fromAccountId(accountId);
+  }
+
+  private void sendEvent(String eventName, @Nullable WritableMap params) {
+    getReactApplicationContext()
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit(eventName, params);
+  }
+
+  @ReactMethod
+  public void sendLumensWithEvents(final String destinationAccountId, final double amount, final String memo, final Promise promise) {
+    if (amount <= 0) {
+      promise.reject(E_AMOUNT_TOO_SMALL, "The amount to be send is less or equal to 0.");
+      return;
+    }
+
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        Network.useTestNetwork();
+        Server server = new Server(HORIZON_TESTNET_URL);
+
+        KeyPair destination = null;
+        try {
+          destination = KeyPair.fromAccountId(destinationAccountId);
+          server.accounts().account(destination);
+        } catch (Exception e) {
+          promise.reject(e);
+        }
+        sendEvent("EV_DESTINATION_ACCOUNT_CHECKED", null);
+
+        AccountResponse sourceAccount = null;
+        try {
+          sourceAccount = server.accounts().account(mKeyPair);
+        } catch (Exception e) {
+          promise.reject(e);
+        }
+        sendEvent("EV_SOURCE_ACCOUNT_UPDATED", null);
+
+        Transaction.Builder txBuilder = new Transaction.Builder(sourceAccount)
+            .addOperation(new PaymentOperation.Builder(destination, new AssetTypeNative(), String.valueOf(amount)).build());
+
+        if (memo != null && memo.length() > 0) {
+          txBuilder.addMemo(Memo.text(memo));
+        }
+
+        Transaction tx = txBuilder.build();
+        tx.sign(mKeyPair);
+        sendEvent("EV_CREATED_TRANSACTION", null);
+
+        try {
+          SubmitTransactionResponse response = server.submitTransaction(tx);
+          promise.resolve(response.getHash());
+        } catch (Exception e) {
+          System.out.println("Something went wrong!");
+          System.out.println(e.getMessage());
+          promise.reject(e);
+          // If the result is unknown (no response body, timeout etc.) we simply resubmit
+          // already built transaction:
+          // SubmitTransactionResponse response = server.submitTransaction(transaction);
+        }
+      }
+    }).start();
   }
 
   @ReactMethod
@@ -39,16 +144,10 @@ public class StellarAccount extends ReactContextBaseJavaModule {
         ret.putString("accountId", pair.getAccountId());
         ret.putString("secretSeed", new String(pair.getSecretSeed()));
 
-        Log.d("STELLAR-ACCOUNT", "Account id: " + pair.getAccountId());
-        Log.d("STELLAR-ACCOUNT", "Secret seed: " + new String(pair.getSecretSeed()));
-
-        String friendbotUrl = String.format(
-            "https://horizon-testnet.stellar.org/friendbot?addr=%s",
-            pair.getAccountId());
+        String friendbotUrl = String.format(HORIZON_TESTNET_URL + "/friendbot?addr=%s", pair.getAccountId());
         InputStream response = null;
         try {
           response = new URL(friendbotUrl).openStream();
-          String body = new Scanner(response, "UTF-8").useDelimiter("\\A").next();
           response.close();
           promise.resolve(ret);
           //successCallback.invoke(relativeX, relativeY, width, height);
@@ -72,7 +171,7 @@ public class StellarAccount extends ReactContextBaseJavaModule {
     new Thread(new Runnable() {
       @Override
       public void run() {
-        Server server = new Server("https://horizon-testnet.stellar.org");
+        Server server = new Server(HORIZON_TESTNET_URL);
         KeyPair pair = KeyPair.fromAccountId(accountId);
         Double xlmBalance = 0.0;
         try {
@@ -99,5 +198,74 @@ public class StellarAccount extends ReactContextBaseJavaModule {
     } catch (Exception e) {
       promise.resolve(false);
     }
+  }
+
+  @ReactMethod
+  public static void existsAccount(final String accountId, Promise promise) {
+    Network.useTestNetwork();
+    Server server = new Server(HORIZON_TESTNET_URL);
+
+    try {
+      KeyPair pair = KeyPair.fromAccountId(accountId);
+      server.accounts().account(pair);
+      promise.resolve(true);
+    } catch (Exception e) {
+      promise.resolve(false);
+    }
+  }
+
+  @ReactMethod
+  public static void sendLumensWithMemo(final String sourceSecretKey,
+                                final String destinationAccountId,
+                                final double amount,
+                                final String memo,
+                                final Promise promise) {
+    if (amount <= 0) {
+      promise.reject(E_AMOUNT_TOO_SMALL, "The amount to be send is less or equal to 0.");
+      return;
+    }
+
+    Network.useTestNetwork();
+    Server server = new Server(HORIZON_TESTNET_URL);
+
+    KeyPair source = KeyPair.fromSecretSeed(sourceSecretKey);
+    KeyPair destination = KeyPair.fromAccountId(destinationAccountId);
+
+    AccountResponse sourceAccount = null;
+    try {
+      sourceAccount = server.accounts().account(source);
+    } catch (Exception e) {
+      promise.reject(e);
+    }
+
+    Transaction.Builder txBuilder = new Transaction.Builder(sourceAccount)
+        .addOperation(new PaymentOperation.Builder(destination, new AssetTypeNative(), String.valueOf(amount)).build());
+
+    if (memo != null && memo.length() > 0) {
+      txBuilder.addMemo(Memo.text(memo));
+    }
+
+    Transaction tx = txBuilder.build();
+    tx.sign(source);
+
+    try {
+      SubmitTransactionResponse response = server.submitTransaction(tx);
+      System.out.println("Success!");
+      System.out.println(response);
+    } catch (Exception e) {
+      System.out.println("Something went wrong!");
+      System.out.println(e.getMessage());
+      // If the result is unknown (no response body, timeout etc.) we simply resubmit
+      // already built transaction:
+      // SubmitTransactionResponse response = server.submitTransaction(transaction);
+    }
+  }
+
+  @ReactMethod
+  public static void sendLumens(final String sourceSecretKey,
+                                final String destinationAccountId,
+                                final double amount,
+                                final Promise promise) {
+    sendLumensWithMemo(sourceSecretKey, destinationAccountId, amount, null, promise);
   }
 }
